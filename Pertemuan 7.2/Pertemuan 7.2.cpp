6d713011e95584c9515f0875b08dfb195d20bf19
gibranrafii <gibranrafi21@gmail.com>
2024-04-23 09:41:10 +0700

    membuat void display

--- Pertemuan 7.2/Pertemuan 7.2.cpp
+++ Pertemuan 7.2/Pertemuan 7.2.cpp
@@ -17,6 +17,12 @@ public:
     float hitungluas() {
         return panjang * lebar;
     }
+
+    void display() {
+        cout << "Panjangnya = " << panjang << endl;
+        cout << "Lebarnya = " << lebar << endl;
+        cout << "Luasnya = " << hitungluas() << endl;
+    }
 };
 
 int main()
